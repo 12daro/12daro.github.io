b01fa9b1567664ec3a65e904727fc7579cfa8576
12daro <33682683+12daro@users.noreply.github.com>
2017-11-29 21:46:30 +0100

    punktacja, samobój, mailing

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/cache-polyfill.js');
 
 
-var version = '0.501711171939';
+var version = '0.501711292';
 var cacheName = 'cache_' + version;
 var dataCacheName = 'cache_data_' + version;
 
