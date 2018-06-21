49947e14c7eed60817c024541bebb87cd5466f51
12daro <33682683+12daro@users.noreply.github.com>
2018-06-21 22:08:50 +0200

    ikona, edytowanie, notatka

--- sw.js
+++ sw.js
@@ -2,12 +2,11 @@
 importScripts('js/cache-polyfill.js');
 
 
-var version = '0.5018031206';
+var version = '0.5018062101';
 var cacheName = 'cache_' + version;
 var dataCacheName = 'cache_data_' + version;
 
 var filesToCache = [
-  '/',
   'index.html',
   'js/app.js',
   'js/jquery.min.js',
